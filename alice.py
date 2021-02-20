71197f5334a2bfbdd4decd4bc1c1b70a1a551cda
LuNeder <luananeder@gmail.com>
2021-02-20 16:04:03 -0300

    Update alice.py

--- alice.py
+++ alice.py
@@ -25,30 +25,51 @@
 
 import keyboard
 import time
+import sys
+
+args = str(sys.argv)
 
 #Asks how many seconds between lines and saves answer as t
-t = int(input("How many seconds between lines? -> "))
-print(t)
+if (args.__contains__("--help")):
+    mode = "help"
+    print("--help: Help mode: show this message")
+    print("--loop: Loop mode: start the file again from beginning after it's over")
+    print("no args: Normal mode: stop once file ends")
+else:
+    t = int(input("How many seconds between lines? -> "))
+    print(t)
 
-#open file
-original = open("original.txt", "r", encoding='utf-8')
-conteudo = original.read()
+    #open file
+    original = open("original.txt", "r", encoding='utf-8')
+    conteudo = original.read()
 
-#divide file lines as list elements and print them
-conteudo_lista = conteudo.splitlines()
-original.close
-print(conteudo_lista)
+    #divide file lines as list elements and print them
+    conteudo_lista = conteudo.splitlines()
+    original.close
+    print(conteudo_lista)
 
-#count how many elements are in the list and print the quantity
-linhas = len(conteudo_lista)
-print(linhas)
+    #count how many elements are in the list and print the quantity
+    linhas = len(conteudo_lista)
+    print(linhas)
 
-#waits 10 seconds so you can find the text input area once Alice is running
-time.sleep(10)
+    #waits 10 seconds so you can find the text input area once Alice is running
+    time.sleep(10)
 
-#types each line and press0 enter, then waits t seconds before doing it with the next line
-for i in range(linhas):
-    keyboard.write(conteudo_lista[i]),
-    time.sleep(0.5),
-    keyboard.press_and_release("enter"),
-    time.sleep(t)
+#types each line and press enter, then waits t seconds before doing it with the next line
+if (args.__contains__("--loop")):
+    mode = "loop"
+    while True:
+        for i in range(linhas):
+            keyboard.write(conteudo_lista[i]),
+            time.sleep(0.5),
+            keyboard.press_and_release("enter"),
+            time.sleep(t)
+elif (args.__contains__("--help")):
+    print("")
+else:
+    mode = "normal"
+    for i in range(linhas):
+        keyboard.write(conteudo_lista[i]),
+        time.sleep(0.5),
+        keyboard.press_and_release("enter"),
+        time.sleep(t)
