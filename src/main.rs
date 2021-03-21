2d08175094c62e039eadf3122649b842b44cd23f
LuNeder <luananeder@gmail.com>
2021-03-21 19:31:49 -0300

    3.0.0 Finished

--- src/main.rs
+++ src/main.rs
@@ -30,16 +30,18 @@ use std::str;
 use std::{thread, time};
 use simulate;
 use simulate::Key;
+use chrono::prelude::*;
 
 fn main() {
-    println!("Not ready yet. Use branch alice-python instead.");
+    let ver: String = "3.0.0".to_string(); //sets ver vaiable to current Alice version
+
     // Get command line input and save it as an args variable
     let args: String = env::args().collect();
 
     // Checks if the mode selected in command line was help and runs it or not
     if args.contains("help") {
 
-        alice_help(); // Help mode
+        alice_help(ver); // Help mode
 
     } else {
 
@@ -65,39 +67,59 @@ fn main() {
     };
 }
 
-// Help mode
-fn alice_help() {
-    println!("help");
+// Help mode, prints help and the version ver
+fn alice_help(ver: String) {
+    println!("Alice {} Help", ver);
+    println!("--help: Help mode: show this message");
+    println!("--loop: Loop mode: restart the file from beginning after it's over");
+    println!("No args: Normal mode: stop once file ends");
 }
 
 // Loop mode
 fn alice_loop(t: i32, divided: Vec<&str>) {
+
     println!("loop ({} seconds between lines)", t);
+
+    // Waits 10 seconds before running
     let tensec = time::Duration::from_secs(10);
     thread::sleep(tensec);
+
+    // Loops Alice
     loop {
-        let mut divided2 = divided.clone();
-        alice(t, divided2);
+        let mut divided2 = divided.clone(); // Clones the divided variable because the program won't compile otherwise
+        alice(t, divided2); // run Alice regular mode, inside the loop
     }
 }
 
 //Regular mode
 fn alice(t: i32, divided: Vec<&str>) {
+
+    // If the args contains loop, does not wait 10 seconds since alice_loop() alreadi did that. If it's regular mode, waits 10 seconds
     let args: String = env::args().collect();
     if args.contains("loop") {println!("LOOP")} else {
         println!("alice ({} seconds between lines)", t);
         let tensec = time::Duration::from_secs(10);
         thread::sleep(tensec);
     }
+
+    // Converts t into u64
     let tttt = t as u64;
+
+    // Repeats with each line of the file
     for i in divided {
-        let halfsec = time::Duration::from_millis(500);
-        simulate::type_str(i).unwrap();
-        thread::sleep(halfsec);
-        simulate::send(Key::Enter).unwrap();
-        let now = time::Instant::now();
-        println!("{}", i);
+
+        let halfsec = time::Duration::from_millis(500); // Set halfsec variable to 500 ms
+
+        simulate::type_str(i).unwrap(); // types the line
+
+        thread::sleep(halfsec); // Waits halfsec (500 ms)
+
+        simulate::send(Key::Enter).unwrap(); // Press enter
+
+        let now = Local::now();
+        println!("{} - {:#?}", i, now); // prints in terminal the linee that was just typed and the time it was sent
+
         let z = time::Duration::from_secs(tttt);
-        thread::sleep(z);
+        thread::sleep(z); // Waits  the time that the user input at the start of the program
     }
 }
