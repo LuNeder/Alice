8d0f6c2b715a72f187217de77a783c3b8658e332
LuNeder <luananeder@gmail.com>
2021-03-21 13:35:16 -0300

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,31 @@
 use std::env;
+use text_io::scan;
 
 fn main() {
     println!("Not ready yet. Use branch alice-python instead.");
+    // Get command line input and save it as an args variable
     let args: String = env::args().collect();
+    // Checks what mode was asked for in command line and runs it
     if args.contains("help") {
-        alice_help();
+        alice_help(); // Help mode
     } else if args.contains("loop") {
-        alice_loop();
+        alice_loop(); // Loop mode
     } else {
-        alice();
+        alice(); // Regular mode
     };
-
 }
+
+// Help mode
 fn alice_help() {
     println!("help");
 }
+
+// Loop mode
 fn alice_loop() {
     println!("loop");
 }
+
+//Regular mode
 fn alice() {
     println!("alice");
 }
