cf8c21dbe992dc49364c32abc614ede7b5ea864e
LuNeder <luananeder@gmail.com>
2021-03-21 13:59:40 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,24 @@ fn main() {
     println!("Not ready yet. Use branch alice-python instead.");
     // Get command line input and save it as an args variable
     let args: String = env::args().collect();
+
     // Checks if the mode selected in command line was help and runs it or not
     if args.contains("help") {
+
         alice_help(); // Help mode
+
     } else {
+
         // Asks how many seconds between lines and save it as a t variable
         println!("How many seconds between lines?");
         let t: i32;
         scan!("{}", t);
         // Prints the t variable
         println!("{} seconds between lines", t);
+
+        //
+        
+
         // Checks selected mode in command line and runs it
         if args.contains("loop") {alice_loop(t);} // Loop mode
         else {alice(t)}; // Regular mode
