2dba8d30034c2c530b17901482ea7fd965a14ceb
LuNeder <luananeder@gmail.com>
2021-03-21 13:20:00 -0300

    Starting to rewrite in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
 fn main() {
     println!("Not ready yet. Use branch alice-python instead.");
+    let args: Vec<String> = env::args().collect();
+    if args.contains("help") {
+        alice_help();
+    } else if args.contains("loop") {
+        alice_loop();
+    } else {
+        alice();
+    };
+
+}
+fn alice_help() {
+    println!("help");
+}
+fn alice_loop() {
+    println!("loop");
+}
+fn alice() {
+    println!("alice");
 }
