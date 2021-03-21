dc4c2343e4fc564aac36f90c10bff4f99686fa9e
LuNeder <luananeder@gmail.com>
2021-03-21 14:04:48 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+// main.rs
+// Copyright 2020~2021 Lu Neder
+//
+// This work may be distributed and/or modified under the
+// conditions of the LaTeX Project Public License, either version 1.3
+// of this license or (at your option) any later version.
+// The latest version of this license is in
+//   http://www.latex-project.org/lppl.txt
+// and version 1.3 or later is part of all distributions of LaTeX
+// version 2005/12/01 or later.
+//
+// This work has the LPPL maintenance status `maintained'.
+//
+// The Current Maintainer of this work is Lu Neder.
+//
+// This work consists of the files main.rs, README.md and the included original.txt example.
+
+//Alice requires an original.txt file on the directory you're running it from.
+//Alice will read the file and then type it (a line followed by an enter, then the next line).
+//This way you can send the file contnent on a chat, for example, each line on a message.
+//You can also tell Alice to wait some time between a line and the other.
+//The first line will be written 10 seconds after you run Alice and tell the time between a line and the other,
+//this way you have time to select the place where you want to input your text.
+//"Alice" was just the 1st name I found on Google lol
+
 use std::env;
 use text_io::scan;
 use std::fs;
@@ -22,7 +47,7 @@ fn main() {
         println!("{} seconds between lines", t);
 
         //
-        
+
 
         // Checks selected mode in command line and runs it
         if args.contains("loop") {alice_loop(t);} // Loop mode
